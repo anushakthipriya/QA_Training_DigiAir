de8362914db57e2c084b2b55c571ca1fc0f4abb5
anushakthipriya <jkanu15@gmail.com>
2022-12-26 12:56:47 +0530

    Add files via upload

--- Signup1.java
+++ Signup1.java
@@ -1,10 +1,15 @@
 package automation1;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -40,15 +45,26 @@ public class Signup1 {
 		driver.findElement(By.xpath("//form// input[@name= 'city']")).sendKeys("Rogers");
 		
 		driver.findElement(By.xpath("//form// input[@name= 'userName']")).sendKeys("Anu123");
-		
-		driver.switchTo().frame(0);
-		
-		WebElement captcha =  driver.findElement(By.xpath("//div//*[@role = 'checkbox']"));
+				
+		new WebDriverWait(driver, 10).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
+					By.xpath("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.google.com/recaptcha')]")));
+		 
+		new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.recaptcha-checkbox-checkmark"))).click();
+		 
+		/*driver.switchTo().frame(0);
+		WebElement captcha =  driver.findElement(By.id("recaptcha-anchor-label"));
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("arguments[0].click()", captcha);
+		*/
 		
+		driver.findElement(By.cssSelector("input[name='stayInformedAboutBizio']")).isSelected();
+		driver.findElement(By.cssSelector("input[name='stayInformedAboutBizio']")).click();
 		
+		driver.findElement(By.cssSelector("input[name='termsConditionsAgreed']")).isSelected();
+		driver.findElement(By.cssSelector("input[name='termsConditionsAgreed']")).click();
 		
+		driver.findElement(By.linkText("Create Account")).click();
+				
 	}
 
 }
